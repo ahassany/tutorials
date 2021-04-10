dd4725f5a47481c5e4e50a91c4adbbd7536fc171
Ahmed El-Hassany <a.hassany@gmail.com>
2021-04-10 08:32:33 +0200

    Initial Empty project

---
+++ dockerCompose/src/main/java/ps/hassany/tutorial/dockerCompose/DockerComposeApplication.java
@@ -0,0 +1,13 @@
+package ps.hassany.tutorial.dockerCompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerComposeApplication.class, args);
+	}
+
+}
---
+++ dockerCompose/src/test/java/ps/hassany/tutorial/dockerCompose/DockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package ps.hassany.tutorial.dockerCompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
