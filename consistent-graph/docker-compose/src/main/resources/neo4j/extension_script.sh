b7340b21d06680bdbaf46edfa87a070fc2dce4b2
Ahmed El-Hassany <a.hassany@gmail.com>
2021-06-04 19:37:38 +0200

    typo

--- consistent-graph/docker-compose/src/main/resources/neo4j/extension_script.sh
+++ consistent-graph/docker-compose/src/main/resources/neo4j/extension_script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo 'CREATE CONSTRAINT BaseIdUnique ON (b:Base) ASSERT b.id IS UNIQUE' | cypher-shell
+echo 'CREATE CONSTRAINT BaseIdUnique ON (b:Base) ASSERT b.id IS UNIQUE' | cypher-shell -u $NEO4J_USER -p $NEO4J_PASSWORD
